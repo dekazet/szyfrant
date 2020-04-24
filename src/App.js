5ce8c31152c2fa7eb038ead9479132f10d09bcb8
Dawid Kozlowski <dawidk@applebox.local>
2020-04-23 20:49:12 -0700

    Basic code entry UI

--- src/App.js
+++ src/App.js
@@ -2,23 +2,55 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class CodeEntryForm extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {codes: ['', '', '']};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChange(codeIndex, event) {
+    const codes = this.state.codes.slice();
+    codes[codeIndex] = event.target.value;
+    this.setState({codes : codes})
+  }
+
+
+  handleSubmit(event) {
+    alert('A code was submitted: ' + this.state.codes);
+    event.preventDefault();
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <ol>
+        <li>
+          <input type="text" value={this.state.codes[0]} onChange={(event) => {this.handleChange(0, event)}} />
+        </li>
+        <li>  
+          <input type="text" value={this.state.codes[1]} onChange={(event) => {this.handleChange(1, event)}} />
+        </li>
+        <li>
+          <input type="text" value={this.state.codes[2]} onChange={(event) => {this.handleChange(2, event)}} />
+        </li> 
+       </ol>
+       <input type="submit" value="Submit" />
+       </form>
+    );
+  }
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="sampleEntry">
+      <CodeEntryForm />
     </div>
   );
 }
